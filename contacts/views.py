84ed28466eb3a00f07afdf48b2891963696e8cb4
Ignacio Fernández <ignacio.fernandez-delano@alstomgroup.com>
2023-09-16 14:29:50 -0300

    search contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+
+def contacts(request):
+    search = request.GET.get("q")
+    if search is not None:
+        users = User.objects.filter(username__icontains=search)
+    else:
+        users = User.objects.all()
+
+    return render(request, "index.html", {"users": users})
