3ce650dc8dab693882af3bebdbe24c45cd7c3d3b
Ignacio Fernández <ignaferdel@gmail.com>
2023-09-18 11:43:34 -0300

    edit + delete functionality

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,4 +1,4 @@
-# contacts/views.py
+# contacts/urls.py
 
 from django.urls import path
 from . import views
@@ -7,4 +7,6 @@ urlpatterns = [
     path("contacts/", views.contacts, name="contacts"),
     path("contacts/new/", views.contacts_new, name="contacts_new"),
     path("contacts/<int:user_id>/", views.contacts_view, name="contacts_view"),
+    path("contacts/<int:user_id>/edit/", views.contacts_edit, name="contacts_edit"),
+    path("contacts/<int:user_id>/delete/", views.contacts_delete, name="contacts_delete"),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,5 @@
 # contacts/views.py
-
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -34,3 +34,28 @@ def contacts_new(request):
 def contacts_view(request, user_id=0):
     user = get_object_or_404(User, id=user_id)
     return render(request, "show.html", {"user": user})
+
+
+def contacts_edit(request, user_id=0):
+    user = get_object_or_404(User, id=user_id)
+
+    if request.method == 'POST':
+        user.username = request.POST.get('username')
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.email = request.POST.get('email')
+        user.save()
+
+        return redirect("/contacts/" + str(user_id))
+    else:
+        return render(request, "edit.html", {"user": user})
+
+
+def contacts_delete(request, user_id=0):
+    if request.method == 'POST':
+        user = get_object_or_404(User, id=user_id)
+        user.delete()
+        messages.add_message(request, messages.SUCCESS, 'Deleted Contact!')
+        return redirect('/contacts')
+    else:
+        return redirect('/contacts')
