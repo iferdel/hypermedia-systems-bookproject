455e164f0f8233c2391476420c3d7b0c4565386e
FERNÁNDEZ DÉLANO Ignacio <ignaferdel@gmail.com>
2023-09-23 13:52:34 -0300

    css transitions in new contacts username and email

--- contacts/views/htmx_views.py
+++ contacts/views/htmx_views.py
@@ -5,17 +5,17 @@ from django.http import HttpResponse
 def check_username(request):
     username = request.POST.get('username')
     if User.objects.filter(username=username).exists():
-        return HttpResponse("<div style='color: red;'>This username already exists</div>")
+        return HttpResponse("<div id='email-error' class='error'>This username already exists</div>")
     else:
-        return HttpResponse("<div style='color: green;'>This username is available</div>")
+        return HttpResponse("<div id='email-error' class='success'>This username is available</div>")
 
 
 def check_email(request):
     email = request.POST.get('email')
     if User.objects.filter(email=email).exists():
-        return HttpResponse("<div style='color: red;'>This email already exists</div>")
+        return HttpResponse("<div id='email-error' class='error'>This email already exists</div>")
     else:
-        return HttpResponse("<div style='color: green;'>This email is available</div>")
+        return HttpResponse("<div id='email-error' class='success'>This email is available</div>")
 
 
 def contacts_count(request):
